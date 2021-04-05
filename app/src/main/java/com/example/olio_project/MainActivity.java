42eca2a59cde143b320fc1a1bc63f4ff6f25573a
Ketsuppimakkara <77271158+Ketsuppimakkara@users.noreply.github.com>
2021-04-05 18:34:28 +0300

    initial version

---
+++ app/src/main/java/com/example/olio_project/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+Object oriented programming project work
+
+Lassi Laukkarinen, Akseli Hulkkonen, Tommmi Paakkunainen
+*/
+
+package com.example.olio_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
